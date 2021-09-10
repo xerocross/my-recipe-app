695e05ce419968e4dab2b8680e5409e5532762d3
Adam Cross <adam@noneya.business>
2021-09-10 16:05:37 -0400

    add initial entities

---
+++ src/main/java/guru/springframework/domain/Ingredient.java
@@ -0,0 +1,16 @@
+package guru.springframework.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Entity
+public class Ingredient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    private BigDecimal amount;
+}
