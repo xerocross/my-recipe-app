437a0f5f801307d2bf52f4dcd62721214176fc42
Adam Cross <adamfgcross@gmail.com>
2021-09-11 07:52:41 -0400

    add data repositories

---
+++ src/main/java/guru/springframework/repositories/CategoryRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import guru.springframework.domain.Category;
+
+public interface CategoryRepository extends CrudRepository<Category, Long> {
+}
